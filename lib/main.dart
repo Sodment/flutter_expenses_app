58e8756793e5e4d14201784f6228cecd56428089
Sodment <sodment@gmail.com>
2021-08-19 18:01:19 +0200

    TestGitIntegration

--- lib/main.dart
+++ lib/main.dart
@@ -80,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Expenses App'),
+        title: Text('Expenses App by Sodment'),
       ),
       body: SingleChildScrollView(
         child: Column(
